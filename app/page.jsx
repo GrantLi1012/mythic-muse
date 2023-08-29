3cdadf70ede7819363f875c49c19a18527148474
Grant Li <ligrant1012@gmail.com>
2023-08-28 20:49:13 -0700

    title in home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,17 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text_center">
+            Discover & Share
+            <br className="max-md:hidden" />
+            <span className="purple_gradient text_center"> AI-Powered Tabletop RPG Prompts</span>
+        </h1>
+        <p className="desc text-center" >
+          MythicMuse is an AI prompting tool for tabletop RPG players to discover, create, share interesting character/npc creation prompts
+        </p>
+
+        {/* Feed */}
+    </section>
   )
 }
 
