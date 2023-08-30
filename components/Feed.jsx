20096e62419079fe09cc7bf42b55e67c858987e7
Grant Li <ligrant1012@gmail.com>
2023-08-29 17:15:29 -0700

    view and copy prompts

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
+"use client";
+
+import {useState, useEffect} from 'react';
+
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({data, handleTagClick}) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+}
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+  const [allPosts, setAllPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+
+  const handleSearchChange = (e) => {
+
+  };
+
+  const fetchPosts = async () => {
+    const res = await fetch("/api/prompt");
+    const data = await res.json();
+
+    setPosts(data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+
   return (
-    <div>Feed</div>
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='Search for a tag or a username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+
+      <PromptCardList
+        data={posts}
+        handleTagClick={() => {}}
+      />
+    </section>
   )
 }
 
